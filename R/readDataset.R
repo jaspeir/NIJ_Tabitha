fdc1b6dc07977edba93ecc45536a513470c5e03f
Endre Palatinus <palatinuse@gmail.com>
2020-01-13 21:08:00 +0100

    Added trial dataset in RData format.

--- R/readDataset.R
+++ R/readDataset.R
@@ -2,3 +2,5 @@ library(readxl)
 
 datasetPath = system.file("extdata", "JS_DRSA_Trial_Key_and_Data_Table.xlsx", package = "DRSA", mustWork = TRUE)
 decisionTable = readxl::read_excel(path = datasetPath, sheet = "Input_Data")
+
+save(decisionTable, file = "data/trial.RData")
