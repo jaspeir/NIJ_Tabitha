ba30dfb5813f0aeb30288cde62f0da9ce80dce62
Endre Palatinus <palatinuse@gmail.com>
2020-01-15 13:35:14 +0100

    Added outranking relation and test cases.

---
+++ tests/testthat/test-outrankingRelation.R
@@ -0,0 +1,48 @@
+library(tibble)
+
+test_that("outranks equal", {
+  r1 = tibble(q = (1))
+  r2 = tibble(q = (1))
+
+  expect_true(outranks(r1, r2, "q"))
+})
+
+test_that("outranks larger", {
+  r1 = tibble(q = (1))
+  r2 = tibble(q = (0))
+
+  expect_true(outranks(r1, r2, "q"))
+})
+
+test_that("does not outrank smaller", {
+  r1 = tibble(q = (1))
+  r2 = tibble(q = (2))
+
+  expect_false(outranks(r1, r2, "q"))
+})
+
+test_that("should fail for empty operands", {
+  r1 = data.frame(q = NULL)
+  r2 = tibble(q = (1))
+
+  expect_error(outranks(r1, r2, "q"))
+  expect_error(outranks(r2, r1, "q"))
+  expect_error(outranks(r1, r1, "q"))
+})
+
+test_that("should fail for missing criterion", {
+  r1 = tibble(q = (1))
+  r2 = tibble(r = (1))
+
+  expect_error(outranks(r1, r2, "q"))
+  expect_error(outranks(r1, r2, "r"))
+  expect_error(outranks(r1, r2, "s"))
+})
+
+test_that("should fail for unequal rows", {
+  r1 = tibble(q = c(1,2))
+  r2 = tibble(q = (1))
+
+  expect_error(outranks(r1, r2, "q"))
+  expect_error(outranks(r2, r1, "q"))
+})
