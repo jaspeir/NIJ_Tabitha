8214245fcb93be34998c7c9ba0a6df2412d373f4
Endre Palatinus <palatinuse@gmail.com>
2020-03-11 09:20:37 +0100

    Added test case for dominance relation considering mixed-attribute types - for now using only dominance attributes.

--- tests/testthat/test-dominanceRelation.R
+++ tests/testthat/test-dominanceRelation.R
@@ -1,8 +1,21 @@
 library(tibble)
+library(purrr)
+
+################################
+### Outranking-attibtes only ###
+################################
+
+setAllAttributesToDominance = function(t) {
+  walk(names(t), function(field) {
+    attributes(t[field])$type = 'dominance'
+    })
+}
 
 test_that("dominates single", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (1))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
 
   expect_true(dominates(r1, r2, c("q")))
 })
@@ -10,6 +23,8 @@ test_that("dominates single", {
 test_that("does not dominate single", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (2))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
 
   expect_false(dominates(r1, r2, c("q")))
 })
@@ -17,6 +32,10 @@ test_that("does not dominate single", {
 test_that("dominates two attributes", {
   r1 = tibble(q = (1), r = (0))
   r2 = tibble(q = (1), r = (0))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
+  attributes(r1$r)$type = 'dominance'
+  attributes(r2$r)$type = 'dominance'
 
   expect_true(dominates(r1, r2, c("q", "r")))
 })
@@ -24,6 +43,10 @@ test_that("dominates two attributes", {
 test_that("does not dominate two attributes", {
   r1 = tibble(q = (1), r = (0))
   r2 = tibble(q = (0), r = (1))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
+  attributes(r1$r)$type = 'dominance'
+  attributes(r2$r)$type = 'dominance'
 
   expect_false(dominates(r1, r2, c("q", "r")))
   expect_false(dominates(r2, r1, c("q", "r")))
@@ -32,6 +55,10 @@ test_that("does not dominate two attributes", {
 test_that("should fail - attribute not in examples", {
   r1 = tibble(q = (1), r = (0))
   r2 = tibble(q = (0), r = (1))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
+  attributes(r1$r)$type = 'dominance'
+  attributes(r2$r)$type = 'dominance'
 
   expect_error(dominates(r1, r2, c("a")))
   expect_error(dominates(r2, r1, c("q", "r", "s")))
@@ -40,6 +67,10 @@ test_that("should fail - attribute not in examples", {
 test_that("vectorized execution - dominates", {
   r1 = tibble(q = c(11, 22, 33), r = c(11, 22, 33))
   r2 = tibble(q = c(1, 2, 3), r = c(1, 2, 3))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
+  attributes(r1$r)$type = 'dominance'
+  attributes(r2$r)$type = 'dominance'
 
   expect_equal(dominates(r1, r2, c("q", "r")), c(TRUE, TRUE, TRUE))
 })
@@ -47,6 +78,10 @@ test_that("vectorized execution - dominates", {
 test_that("vectorized execution - permuting elements of criteria set does not change results", {
   r1 = tibble(q = c(1, 2), r = c(1, 2))
   r2 = tibble(q = c(2, 1), r = c(2, 1))
+  attributes(r1$q)$type = 'dominance'
+  attributes(r2$q)$type = 'dominance'
+  attributes(r1$r)$type = 'dominance'
+  attributes(r2$r)$type = 'dominance'
 
   expect_equal(dominates(r1, r2, c("q", "r")), c(FALSE, TRUE))
   expect_equal(dominates(r1, r2, c("r", "q")), c(FALSE, TRUE))
