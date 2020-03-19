62a5109dc6c4e8b86656a7ebb8b45d78fd3ce82f
Endre Palatinus <palatinuse@gmail.com>
2020-03-19 23:22:18 +0100

    Adapted test-cases for class-unions.

--- tests/testthat/test-downwardClassUnion.R
+++ tests/testthat/test-downwardClassUnion.R
@@ -2,62 +2,68 @@ library(tibble)
 
 test_that("empty dataset", {
   examples = tribble(
-    ~object, ~decision
+    ~object, ~attribute, ~decision
   )
 
-  expect_equal(downwardClassUnion(examples, 1), vector())
+  it = InformationTable$new(examples)
+  expect_equal(it$downwardClassUnion(1), vector())
 })
 
 
 test_that("a single class only", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1
+    ~object, ~attribute, ~decision,
+    1, NA, 1
   )
 
-  expect_equal(downwardClassUnion(examples, 1), c(1))
+  it = InformationTable$new(examples)
+  expect_equal(it$downwardClassUnion(1), c(1))
 })
 
 test_that("a single class only - v2", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 1
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 1
   )
 
-  expect_equal(downwardClassUnion(examples, 1), c(1, 2))
+  it = InformationTable$new(examples)
+  expect_equal(it$downwardClassUnion(1), c(1, 2))
 })
 
 test_that("multiple classes - v1", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 2,
-    3, 3
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 2,
+    3, NA, 3
   )
 
-  expect_equal(downwardClassUnion(examples, 3), c(1, 2, 3))
+  it = InformationTable$new(examples)
+  expect_equal(it$downwardClassUnion(3), c(1, 2, 3))
 })
 
 test_that("multiple classes - v1", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 2,
-    3, 3
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 2,
+    3, NA, 3
   )
 
-  expect_equal(downwardClassUnion(examples, 2), c(1, 2))
+  it = InformationTable$new(examples)
+  expect_equal(it$downwardClassUnion(2), c(1, 2))
 })
 
 
 test_that("multiple classes - v3", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 2,
-    3, 3
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 2,
+    3, NA, 3
   )
 
-  expect_equal(downwardClassUnion(examples, 1), c(1))
+  it = InformationTable$new(examples)
+  expect_equal(it$downwardClassUnion(1), c(1))
 })
--- tests/testthat/test-upwardClassUnion.R
+++ tests/testthat/test-upwardClassUnion.R
@@ -2,62 +2,66 @@ library(tibble)
 
 test_that("empty dataset", {
   examples = tribble(
-    ~object, ~decision
+    ~object, ~attribute, ~decision
   )
-
-  expect_equal(upwardClassUnion(examples, 1), vector())
+  it = InformationTable$new(examples)
+  expect_equal(it$upwardClassUnion(1), vector())
 })
 
 
-test_that("a single class only", {
+test_that("a single class only - v1", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1
+    ~object, ~attribute, ~decision,
+    1, NA, 1
   )
-
-  expect_equal(upwardClassUnion(examples, 1), c(1))
+  it = InformationTable$new(examples)
+  expect_equal(it$upwardClassUnion(1), c(1))
 })
 
 test_that("a single class only - v2", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 1
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 1
   )
 
-  expect_equal(upwardClassUnion(examples, 1), c(1, 2))
+  it = InformationTable$new(examples)
+  expect_equal(it$upwardClassUnion(1), c(1, 2))
 })
 
 test_that("multiple classes - v1", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 2,
-    3, 3
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 2,
+    3, NA, 3
   )
 
-  expect_equal(upwardClassUnion(examples, 3), c(3))
+  it = InformationTable$new(examples)
+  expect_equal(it$upwardClassUnion(3), c(3))
 })
 
-test_that("multiple classes - v1", {
+test_that("multiple classes - v2", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 2,
-    3, 3
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 2,
+    3, NA, 3
   )
 
-  expect_equal(upwardClassUnion(examples, 2), c(2, 3))
+  it = InformationTable$new(examples)
+  expect_equal(it$upwardClassUnion(2), c(2, 3))
 })
 
 
 test_that("multiple classes - v3", {
   examples = tribble(
-    ~object, ~decision,
-    1, 1,
-    2, 2,
-    3, 3
+    ~object, ~attribute, ~decision,
+    1, NA, 1,
+    2, NA, 2,
+    3, NA, 3
   )
 
-  expect_equal(upwardClassUnion(examples, 1), c(1, 2, 3))
+  it = InformationTable$new(examples)
+  expect_equal(it$upwardClassUnion(1), c(1, 2, 3))
 })
