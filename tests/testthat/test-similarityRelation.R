763791b5bd59e590d0f1d6e8f0b399053befa814
Endre Palatinus <palatinuse@gmail.com>
2020-03-19 18:32:10 +0100

    Adapted similarity operator test cases to information table class.

--- tests/testthat/test-similarityRelation.R
+++ tests/testthat/test-similarityRelation.R
@@ -1,107 +1,107 @@
 library(tibble)
 
+createTestDataset = function(X, Y, alpha, beta) {
+  n = nrow(X)
 
-test_that("should fail - missing both", {
-  r1 = tibble(q = (1))
-  r2 = tibble(q = (1))
+  decisionTable = bind_rows(
+    bind_cols(
+      data.frame(
+        object = 1:n,
+        decision = NA,
+        stringsAsFactors = F
+      ),
+      X
+    ),
+    bind_cols(
+      data.frame(
+        object = (n + 1):(2 * n),
+        decision = NA,
+        stringsAsFactors = F
+      ),
+      Y
+    )
+  )
 
-  expect_error(similar(r1, r2, c("q")))
-})
+  metaData = tribble(
+    ~name, ~type, ~alpha, ~beta,
+    'object', 'object', NA, NA,
+    'decision', 'decision', NA, NA
+  )
+  metaData = bind_rows(
+    metaData,
+    data.frame(
+      name = names(X),
+      type = 'similarity',
+      alpha = alpha,
+      beta = beta,
+      stringsAsFactors = F
+    )
+  )
 
-test_that("should fail - missing alpha", {
-  r1 = tibble(q = (1))
-  r2 = tibble(q = (1))
-  attributes(r2$q)$beta = 0.0
-
-  expect_error(similar(r1, r2, c("q")))
-})
-
-test_that("should fail - missing beta", {
-  r1 = tibble(q = (1))
-  r2 = tibble(q = (1))
-  attributes(r2$q)$alpha = 0.0
-
-  expect_error(similar(r1, r2, c("q")))
-})
-
-test_that("should fail - attribute not in examples", {
-  r1 = tibble(q = (1), r = (0))
-  r2 = tibble(q = (0), r = (1))
-
-  expect_error(dominates(r1, r2, c("a")))
-  expect_error(dominates(r2, r1, c("q", "r", "s")))
-})
+  return(InformationTable$new(decisionTable, metaData))
+}
 
 
-# New:
 test_that("similar single diff 0 - v1", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (1))
-  attributes(r2$q)$alpha = 0.0
-  attributes(r2$q)$beta = 0.0
+  it = createTestDataset(r1, r2, alpha = 0.0, beta = 0.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff 0 - v2", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (1))
-  attributes(r2$q)$alpha = 1.0
-  attributes(r2$q)$beta = 0.0
+  it = createTestDataset(r1, r2, alpha = 1.0, beta = 0.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff 0 - v3", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (1))
-  attributes(r2$q)$alpha = 0.0
-  attributes(r2$q)$beta = 1.0
+  it = createTestDataset(r1, r2, alpha = 0.0, beta = 1.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff 0 - v4", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (1))
-  attributes(r2$q)$alpha = 1.0
-  attributes(r2$q)$beta = 1.0
+  it = createTestDataset(r1, r2, alpha = 1.0, beta = 1.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff nonzero - v1", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (2))
-  attributes(r2$q)$alpha = 0.0
-  attributes(r2$q)$beta = 0.0
+  it = createTestDataset(r1, r2, alpha = 0.0, beta = 0.0)
 
-  expect_false(similar(r1, r2, c("q")))
+  expect_false(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff nonzero - v2", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (2))
-  attributes(r2$q)$alpha = 1.0
-  attributes(r2$q)$beta = 0.0
+  it = createTestDataset(r1, r2, alpha = 1.0, beta = 0.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff nonzero - v3", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (2))
-  attributes(r2$q)$alpha = 0.0
-  attributes(r2$q)$beta = 1.0
+  it = createTestDataset(r1, r2, alpha = 0.0, beta = 1.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
 
 test_that("similar single diff nonzero - v4", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (2))
-  attributes(r2$q)$alpha = 1.0
-  attributes(r2$q)$beta = 1.0
+  it = createTestDataset(r1, r2, alpha = 1.0, beta = 1.0)
 
-  expect_true(similar(r1, r2, c("q")))
+  expect_true(it$similar(it$decisionTable[1,], it$decisionTable[2,], c("q")))
 })
