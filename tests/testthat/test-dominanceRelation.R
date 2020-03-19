e31bb909abf476a5ffb28196b28450e0d21b0e12
Endre Palatinus <palatinuse@gmail.com>
2020-03-19 19:24:29 +0100

    Adapted test-case for dominance-relation - dominance-attributes only.

--- tests/testthat/test-dominanceRelation.R
+++ tests/testthat/test-dominanceRelation.R
@@ -1,89 +1,91 @@
 library(tibble)
 library(purrr)
 
-################################
-### Outranking-attibtes only ###
-################################
+#################################
+### Dominance-attributes only ###
+#################################
 
-setAllAttributesToDominance = function(t) {
-  walk(names(t), function(field) {
-    attributes(t[field])$type = 'dominance'
-    })
+library(tibble)
+
+createTestDataset = function(X, Y) {
+  n = nrow(X)
+
+  decisionTable = bind_rows(
+    bind_cols(
+      data.frame(
+        object = 1:n,
+        decision = NA,
+        stringsAsFactors = F
+      ),
+      X
+    ),
+    bind_cols(
+      data.frame(
+        object = (n + 1):(2 * n),
+        decision = NA,
+        stringsAsFactors = F
+      ),
+      Y
+    )
+  )
+
+  metaData = tribble(
+    ~name, ~type, ~alpha, ~beta,
+    'object', 'object', NA, NA,
+    'decision', 'decision', NA, NA
+  )
+  metaData = bind_rows(
+    metaData,
+    data.frame(
+      name = names(X),
+      type = 'dominance',
+      alpha = NA,
+      beta = NA,
+      stringsAsFactors = F
+    )
+  )
+
+  return(InformationTable$new(decisionTable, metaData))
 }
 
 test_that("dominates single", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (1))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
+  it = createTestDataset(r1, r2)
 
-  expect_true(dominates(r1, r2, c("q")))
+  expect_true(it$dominates(1, 2, "q"))
 })
 
 test_that("does not dominate single", {
   r1 = tibble(q = (1))
   r2 = tibble(q = (2))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
+  it = createTestDataset(r1, r2)
 
-  expect_false(dominates(r1, r2, c("q")))
+  expect_false(it$dominates(1, 2, "q"))
 })
 
 test_that("dominates two attributes", {
   r1 = tibble(q = (1), r = (0))
   r2 = tibble(q = (1), r = (0))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
-  attributes(r1$r)$type = 'dominance'
-  attributes(r2$r)$type = 'dominance'
+  it = createTestDataset(r1, r2)
 
-  expect_true(dominates(r1, r2, c("q", "r")))
+  expect_true(it$dominates(1, 2, c("q", "r")))
 })
 
 test_that("does not dominate two attributes", {
   r1 = tibble(q = (1), r = (0))
   r2 = tibble(q = (0), r = (1))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
-  attributes(r1$r)$type = 'dominance'
-  attributes(r2$r)$type = 'dominance'
+  it = createTestDataset(r1, r2)
 
-  expect_false(dominates(r1, r2, c("q", "r")))
-  expect_false(dominates(r2, r1, c("q", "r")))
+  expect_false(it$dominates(1, 2, c("q", "r")))
+  expect_false(it$dominates(2, 1, c("q", "r")))
 })
 
 test_that("should fail - attribute not in examples", {
   r1 = tibble(q = (1), r = (0))
   r2 = tibble(q = (0), r = (1))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
-  attributes(r1$r)$type = 'dominance'
-  attributes(r2$r)$type = 'dominance'
+  it = createTestDataset(r1, r2)
 
-  expect_error(dominates(r1, r2, c("a")))
-  expect_error(dominates(r2, r1, c("q", "r", "s")))
+  expect_error(it$dominates(1, 2, c("a")))
+  expect_error(it$dominates(1, 2, c("q", "r", "s")))
 })
-
-test_that("vectorized execution - dominates", {
-  r1 = tibble(q = c(11, 22, 33), r = c(11, 22, 33))
-  r2 = tibble(q = c(1, 2, 3), r = c(1, 2, 3))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
-  attributes(r1$r)$type = 'dominance'
-  attributes(r2$r)$type = 'dominance'
-
-  expect_equal(dominates(r1, r2, c("q", "r")), c(TRUE, TRUE, TRUE))
-})
-
-test_that("vectorized execution - permuting elements of criteria set does not change results", {
-  r1 = tibble(q = c(1, 2), r = c(1, 2))
-  r2 = tibble(q = c(2, 1), r = c(2, 1))
-  attributes(r1$q)$type = 'dominance'
-  attributes(r2$q)$type = 'dominance'
-  attributes(r1$r)$type = 'dominance'
-  attributes(r2$r)$type = 'dominance'
-
-  expect_equal(dominates(r1, r2, c("q", "r")), c(FALSE, TRUE))
-  expect_equal(dominates(r1, r2, c("r", "q")), c(FALSE, TRUE))
-})
-
