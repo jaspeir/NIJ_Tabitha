c8f4a0b7b21e8cee03775abd87159f0d177d2c96
Endre Palatinus <palatinuse@gmail.com>
2020-03-27 17:50:47 +0100

    Bugfix: isSubset - support empty sets - correctly handle vectors

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -12,8 +12,13 @@ convertMatrixToList = function(m, objects) {
 #' @param A the left operand - vector
 #' @param B the right operand - vector
 #' @return the boolean value of the operation
+#' @export
 isSubset = function(A, B) {
   stopifnot(length(A) == length(B))
 
-  return(A & B == A)
+  if (all(!A) && all(!B)) {
+    return(TRUE)
+  }
+
+  return(all((A & B) == A))
 }
