867515f51c320f276040721a800e7dc9dece3933
Endre Palatinus <palatinuse@gmail.com>
2020-03-27 10:55:56 +0100

    Added helper method to check whether set A is a subset of set B.

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -7,3 +7,13 @@ convertMatrixToList = function(m, objects) {
 
   map(1:nrow(m), function(r) { objects[m[r, ]] })
 }
+
+#' Function to determine whether A is a subset of B.
+#' @param A the left operand - vector
+#' @param B the right operand - vector
+#' @return the boolean value of the operation
+isSubset = function(A, B) {
+  stopifnot(length(A) == length(B))
+
+  return(A & B == A)
+}
