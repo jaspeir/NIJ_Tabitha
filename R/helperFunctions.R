b5a354f1582db93cb358b8abd72f6a429e76266e
Endre Palatinus <palatinuse@gmail.com>
2020-03-27 18:36:23 +0100

    Added isSubset function for ordinary sets.

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -8,7 +8,7 @@ convertMatrixToList = function(m, objects) {
   map(1:nrow(m), function(r) { objects[m[r, ]] })
 }
 
-#' Function to determine whether A is a subset of B.
+#' Function to determine whether A is a subset of B - bool vectors.
 #' @param A the left operand - vector
 #' @param B the right operand - vector
 #' @return the boolean value of the operation
@@ -21,4 +21,18 @@ isSubset = function(A, B) {
   }
 
   return(all((A & B) == A))
+}
+
+#' Function to determine whether A is a subset of B - arbitrary sets.
+#' @param A the left operand - vector
+#' @param B the right operand - vector
+#' @return the boolean value of the operation
+#' @export
+isSubsetArbitrary = function(A, B) {
+
+  if (length(A) == 0 && length(B) == 0) {
+    return(TRUE)
+  }
+
+  return(setequal(intersect(A, B), A))
 }
