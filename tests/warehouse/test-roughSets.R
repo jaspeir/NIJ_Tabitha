e4809e7df4087348efabc25493fa2d8e65b6e28a
Endre Palatinus <palatinuse@gmail.com>
2020-03-28 12:57:20 +0100

    Added tests for acc and quality for the warehouse dataset.

--- tests/warehouse/test-roughSets.R
+++ tests/warehouse/test-roughSets.R
@@ -37,3 +37,14 @@ test_that("test boundary of upward union t=2", {
   expect_equal(getObjects(boundary$upward[2,]), c("w3", "w4", "w6", "w7", "w8"))
 })
 
+test_that("test accuracy of approximation", {
+  acc = warehouseIT$accuracyOfApproximation(roughSets)
+  expect_equal(round(acc$downward[1], digits = 2), 0.17)
+  expect_equal(round(acc$upward[2], digits = 2), 0.29)
+})
+
+test_that("test quality of classification", {
+  q = warehouseIT$qualityOfApproximation(boundaryRegions = boundary)
+  expect_equal(round(q - 0.0000001, digits = 2), 0.37)
+})
+
