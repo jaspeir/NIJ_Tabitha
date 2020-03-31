76e7217a90ef182ea3b86790a5766719ab1feb72
Endre Palatinus <palatinuse@gmail.com>
2020-03-31 12:21:04 +0200

    Adapted test cases.

--- tests/testthat/test-elementaryCondition.R
+++ tests/testthat/test-elementaryCondition.R
@@ -1,34 +1,41 @@
+#load("data/trial-informationTable.RData")
+trialIT = InformationTable$new(informationTable$decisionTable, informationTable$metaData)
+
 test_that("should succeed", {
-  ElementaryCondition$new(attribute = "Q1", value = 3, it = informationTable, isLowerBound = F)
+  ElementaryCondition$new(attribute = "Q1", value = 80, it = trialIT, isLowerBound = NA)
   expect_true(TRUE)
 })
 
+test_that("should fail - NA lower bound for dominance", {
+  expect_error(ElementaryCondition$new(attribute = "Q3OD", value = 'Value for association', it = trialIT, isLowerBound = NA))
+})
+
 test_that("empty cover - ind", {
-  e = ElementaryCondition$new(attribute = "QF2", value = 0, it = informationTable, isLowerBound = F)
-  expect_equal(e$elementCover(informationTable), character())
+  e = ElementaryCondition$new(attribute = "QF3PD", value = "Yes", it = trialIT, isLowerBound = NA)
+  expect_equal(e$elementCover(trialIT), character())
 })
 
 test_that("empty cover - sim", {
-  e = ElementaryCondition$new(attribute = "Q1", value = 0, it = informationTable, isLowerBound = F)
-  expect_equal(e$elementCover(informationTable), character())
+  e = ElementaryCondition$new(attribute = "Q1", value = 0, it = trialIT, isLowerBound = NA)
+  expect_equal(e$elementCover(trialIT), character())
 })
 
 test_that("empty cover - dom", {
-  e = ElementaryCondition$new(attribute = "Q3OD", value = 10, it = informationTable, isLowerBound = T)
-  expect_equal(e$elementCover(informationTable), character())
+  e = ElementaryCondition$new(attribute = "Q3OD", value = 'Insufficient Detail', it = trialIT, isLowerBound = F)
+  expect_equal(e$elementCover(trialIT), character())
 })
 
 test_that("non-empty cover - ind", {
-  e = ElementaryCondition$new(attribute = "QF3ST", value = 9, it = informationTable, isLowerBound = F)
-  expect_equal(e$elementCover(informationTable), c("X12", "X15", "X23", "X24"))
+  e = ElementaryCondition$new(attribute = "QF3ST", value = "Yes", it = trialIT, isLowerBound = NA)
+  expect_equal(e$elementCover(trialIT), c("X12", "X15", "X23", "X24"))
 })
 
 test_that("non-empty cover - sim", {
-  e = ElementaryCondition$new(attribute = "Q2", value = 22, it = informationTable, isLowerBound = F)
-  expect_equal(e$elementCover(informationTable), informationTable$objects)
+  e = ElementaryCondition$new(attribute = "Q2", value = 22, it = trialIT, isLowerBound = NA) # TODO: 22 = "Moderate"
+  expect_equal(e$elementCover(trialIT), trialIT$objects)
 })
 
 test_that("non-empty cover - dom", {
-  e = ElementaryCondition$new(attribute = "Q3OTHVAL", value = 2, it = informationTable, isLowerBound = T)
-  expect_equal(e$elementCover(informationTable), c("X13", "X16", "X30", "X35", "X36"))
+  e = ElementaryCondition$new(attribute = "Q3OTHVAL", value = "Value for exclusion", it = trialIT, isLowerBound = T)
+  expect_equal(e$elementCover(trialIT), c("X13", "X16", "X30", "X35", "X36"))
 })
